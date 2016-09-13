4aa055db963dba8314ea3b9f819d915ef680954f
Harrison Harnisch <hharnisc@gmail.com>
2016-09-12 21:11:42 -0500

    push image out to kubernetes

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 npm i && \
 npm t && \
 docker build -t hharnisc/surf-board:$SURF_SHA1 . && \
-docker push hharnisc/surf-board:$SURF_SHA1
+docker push hharnisc/surf-board:$SURF_SHA1 && \
+kubectl set image deployment/surfboard surfboard=hharnisc/surf-board:$SURF_SHA1
